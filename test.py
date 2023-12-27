26695814ad07ac9a5ba5a03cb62b16fde0cee371
Ashish Alex <aalex@jaguarlandrover.com>
2023-12-27 19:35:38 +0000

    multiply three numbers

--- test.py
+++ test.py
@@ -3,3 +3,7 @@ def add_three_numbers(a, b, c):
     """Add three numbers together."""
     return a + b + c
 
+def multiply_three_numbers(a, b, c):
+    """Multiply three numbers together."""
+    return a * b * c
+
