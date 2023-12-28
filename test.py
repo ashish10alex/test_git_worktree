db50a8ab4d314b3fc62691a720cb6d7d56e55ed6
Ashish Alex <aalex@jaguarlandrover.com>
2023-12-28 15:46:18 +0000

    binary search in main branch

--- test.py
+++ test.py
@@ -19,3 +19,23 @@ def algo_substraction(a, b, c):
 def algo_addition(a, b, c):
     """ weird addition """
     return multiply_three_numbers(a, b, c) + add_three_numbers(a, b, c)
+
+def binary_search(list, item):
+    """Binary search algorithm."""
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+
+        if guess > item:
+            high = mid - 1
+
+        else:
+            low = mid + 1
+
+    return None
